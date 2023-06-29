b8a3373076e1747002180487a1d243e5861a1c02
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-06-29 12:12:48 +0900

    1
    1

--- src/app/movie/page.jsx
+++ src/app/movie/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.scss";
 const Movie = () => {
-    return <div>Movie</div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=9278d13f704ad0fe53c2263b692efd89"
+      );
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await res.json();
+      setMovies(data.results);
+    };
+    getData();
+  }, []);
+  console.log(movies);
+  return <div className={styles.cont}>Movie</div>;
 };
-
 export default Movie;
